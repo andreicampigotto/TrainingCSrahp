e55182b6030c8440cabbc000eefb0040558ada36
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-23 17:22:42 -0300

    Commit e era isso

---
+++ MeuCarro4x4Class/MeuCarro4x4Class/MyFirstClass/Carro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCarro4x4Class.MyFirstClass
+{
+	class Carro
+	{
+		int rodas = 0;
+		int portas = 0;
+		int bacos = 0;
+		int janelas = 0;
+
+		public void MostrarCarro()
+		{
+			Console.WriteLine($"Nosso carro: " +
+				$"Quant Rodas [{rodas}] " +
+				$"Quant Portas [{portas}] " +
+				$"Quant Bancos [{bacos}] " +
+				$"Quant Janelas [{janelas}]");
+		}
+	}
+}
---
+++ MeuCarro4x4Class/MeuCarro4x4Class/Program.cs
@@ -0,0 +1,19 @@
+﻿using MeuCarro4x4Class.MyFirstClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCarro4x4Class
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var Teste = new Carro();
+			Teste.MostrarCarro();
+			Console.ReadKey();
+		}
+	}
+}
