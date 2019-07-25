80655705f3ef171293239207968eb58e36727355
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-25 10:20:28 -0300

    Commit e era isso

---
+++ BoletimEscolar/AlunoNota/Program.cs
@@ -0,0 +1,44 @@
+﻿using BoletimEscolar.CalculaNotas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlunoNota
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CalculaNotas calcula = new CalculaNotas();
+
+			double media, n1, n2, n3;
+			Console.WriteLine("Insira a primeira notas do aluno: ");
+			n1 = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Insira a segunda notas do aluno: ");
+			n2 = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Insira a terceira notas do aluno: ");
+			n3 = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine($"Média: {media = calcula.CalculaMedia(n1, n2, n3)}");	
+
+			int assiduidade, aulas, faltas;
+
+			Console.WriteLine("Insira a quantidade de aulas total: ");
+			aulas = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Insira a quantidade de faltas do aluno: ");
+			faltas = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine($"Assiduidade: {assiduidade = calcula.CalculaPresencas(aulas, faltas)}%");
+
+			Console.WriteLine("Situação do aluno: "); 
+				calcula.Situacao(media, assiduidade);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ BoletimEscolar/BoletimEscolar/Calcula_Notas/CalculaNotas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletimEscolar.CalculaNotas
+{
+	public class CalculaNotas
+	{
+		public double CalculaMedia(double nota1, double nota2, double nota3)
+		{
+			return ((nota1 + nota2 + nota3) / 3);
+		}
+
+		public int CalculaPresencas(int quantidadeAulas, int quantidadeFaltas)
+		{
+			return (100 - (quantidadeFaltas * 100) / quantidadeAulas);
+		}
+
+		public void Situacao(double media, int assiduidade)
+		{
+			if (media > 6.99 && assiduidade >= 75)
+				Console.WriteLine($"Aprovado, média: {media} , assiduidade: {assiduidade}%");
+			else
+				Console.WriteLine($"Reprovado, média: {media} , assiduidade: {assiduidade}%");
+
+		}
+	}
+}
