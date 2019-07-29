7f9d9c0ac90929f38fd5d2c19fa5364717a64aaf
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 09:57:33 -0300

    Exemplo encapsulamento

---
+++ BancoCeSharp/BancoCeSharp/Classes/Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoCeSharp.Classes
+{
+	class Conta
+	{
+		double saldo = 0;
+
+		//Propriedade que contem o valor de saldo
+		public double Saldo { get { return saldo; } }
+
+		public Conta()
+		{
+			//Bonus de mil reais para iniciar operar daytrade
+			saldo = 1000;
+		}
+
+		/// <summary>
+		/// Método para sacar de acordo com o saldo disponivel
+		/// </summary>
+		/// <param name="valor">Valor para sacar</param>
+		/// <returns>retorna se o saque foi feito ou não</returns>
+		public bool Sacar(double valor)
+		{
+
+			if (valor <= saldo)
+			{// Desconta do valor do saldo na conta
+				saldo -= valor;
+				return true;
+			}
+			//retorna false em caso de não conter saldo disponivel
+			return false;	
+		}
+		
+		/// <summary>
+		/// Metodo que mostra saldo em conta
+		/// </summary>
+		public double MostraSaldo()
+		{
+			return saldo;
+		}
+	}
+}
---
+++ BancoCeSharp/BancoCeSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using BancoCeSharp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoCeSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Conta accountUser = new Conta();
+
+			Console.WriteLine("Informe o valor para saque: ");
+			//Aqui usa a conversão para doble do valor informado
+			double.TryParse(Console.ReadLine(), out double valorSacar);
+			//verifica se o valor é maior que 0 e se realizou o saque
+			if (valorSacar > 0 && accountUser.Sacar(valorSacar))
+					Console.WriteLine("Saque realizado com sucesso!");
+			else
+					Console.WriteLine("Operação não pode ser realizada");
+
+			//Mostra saldo disponivel
+			Console.WriteLine($"Saldo atual disponivel: {accountUser.Saldo.ToString("N2")}");
+
+			Console.ReadKey();
+		}
+	}
+}
