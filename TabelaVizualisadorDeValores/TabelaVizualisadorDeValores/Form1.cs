3e7cee3d37d2ab2b8ca97399e0f89a9b111da7d0
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-30 16:37:39 -0300

    olha no do felipe

---
+++ TabelaVizualisadorDeValores/TabelaVizualisadorDeValores/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TabelaVizualisadorDeValores.Model;
+
+namespace TabelaVizualisadorDeValores
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		List<Contrac> listContracs = new List<Contrac>();
+
+		private void BtAdiciona_Click(object sender, EventArgs e)
+		{
+			listContracs.Add(new Contrac()
+			{
+				Id = listContracs.Count,
+				Value = new Random().Next(100),
+				DatInc = DateTime.Now
+			});
+
+			dataGridView1.DataSource = null;
+			dataGridView1.DataSource = listContracs;
+		}
+
+		private void DataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+	}
+}
---
+++ TabelaVizualisadorDeValores/TabelaVizualisadorDeValores/Model/Contrac.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabelaVizualisadorDeValores.Model
+{
+	public class Contrac
+	{
+		public int Id { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Value { get; set; }
+
+		public DateTime DatInc { get; set; }
+	}
+}
