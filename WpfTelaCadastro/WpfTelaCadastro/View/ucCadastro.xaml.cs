d5ac965e50ff94eb3636078612491c95c82f5e76
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-08 11:27:57 -0300

    front e inicio de regex c# match
    terminar validação de regex c# match

---
+++ WpfTelaCadastro/WpfTelaCadastro/View/ucCadastro.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTelaCadastro.View
+{
+	/// <summary>
+	/// Interaction logic for ucCadastro.xaml
+	/// </summary>
+	public partial class ucCadastro : UserControl
+	{
+		public ucCadastro()
+		{
+			InitializeComponent();
+		}
+
+		public event EventHandler TRY;
+
+		private void tbNome_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter)
+			{
+				tbTelefone.Focus();
+			}
+		}
+
+		private void TbTelefone_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter)
+			{
+				tbEMail.Focus();
+			}
+		}
+
+		private void TbEMail_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.Key == Key.Enter)
+			{
+
+			}
+		}
+
+		private void Reg()
+		{
+
+		}
+	}
+}
