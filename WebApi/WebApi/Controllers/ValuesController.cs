fc5c829e31eac0a4cedc81de804c016f347b398f
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-16 08:23:55 -0300

    web api
    web api para tentar entender

---
+++ WebApi/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+	[EnableCors(origins: "*", headers: "*", methods: "*")]
+	public class ValuesController : ApiController
+	{
+		LocacaoDB locacaoDB = new LocacaoDB();
+		// GET api/values
+
+		public List<Carro> Get() {
+			return locacaoDB.Carros.ToList<Carro>();
+		}
+
+		/*public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}*/
+
+		// GET api/values/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/values
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT api/values/5
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/values/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
