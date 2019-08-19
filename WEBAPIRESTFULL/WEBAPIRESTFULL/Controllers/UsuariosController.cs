ccdcf1ec3306f17e94ab4e21b68996a638c19590
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-19 08:30:24 -0300

    Mudanças dos Controllers
    arrumando os controllers

--- WEBAPIRESTFULL/WEBAPIRESTFULL/Controllers/UsuariosController.cs
+++ WEBAPIRESTFULL/WEBAPIRESTFULL/Controllers/UsuariosController.cs
@@ -11,113 +11,113 @@ using System.Web.Http.Description;
 using WEBAPIRESTFULL.Models;
 using System.Web.Http.Cors;
 
-    
+
 namespace WEBAPIRESTFULL.Controllers
 {
-    [EnableCors(origins:"*",headers:"*",methods:"*")]
-    public class UsuariosController : ApiController
-    {
-        private readonly BibliotecaContextDB db = new BibliotecaContextDB();
+	[EnableCors(origins: "*", headers: "*", methods: "*")]
+	public class UsuariosController : ApiController
+	{
+		private readonly BibliotecaContextDB db = new BibliotecaContextDB();
 
-        // GET: api/Usuarios
-        public IQueryable<Usuarios> GetUsuarios()
-        {
-            return db.Usuarios.Where(x => x.Ativo == true);
-        }
+		// GET: api/Usuarios
+		public IQueryable<Usuarios> GetUsuarios()
+		{
+			return db.Usuarios.Where(x => x.Ativo == true);
+		}
 
-        // GET: api/Usuarios/5
-        [ResponseType(typeof(Usuarios))]
-        public IHttpActionResult GetUsuarios(int id)
-        {
-            Usuarios usuarios = db.Usuarios.Find(id);
-            if (usuarios == null)
-            {
-                return NotFound();
-            }
+		// GET: api/Usuarios/5
+		[ResponseType(typeof(Usuarios))]
+		public IHttpActionResult GetUsuarios(int id)
+		{
+			Usuarios usuarios = db.Usuarios.Find(id);
+			if (usuarios == null)
+			{
+				return NotFound();
+			}
 
-            return Ok(usuarios);
-        }
+			return Ok(usuarios);
+		}
 
-        // PUT: api/Usuarios/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutUsuarios(int id, Usuarios usuarios)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+		// PUT: api/Usuarios/5
+		[ResponseType(typeof(void))]
+		public IHttpActionResult PutUsuarios(int id, Usuarios usuarios)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
 
-            if (id != usuarios.Id)
-            {
-                return BadRequest();
-            }
+			if (id != usuarios.Id)
+			{
+				return BadRequest();
+			}
 
-            db.Entry(usuarios).State = EntityState.Modified;
+			db.Entry(usuarios).State = EntityState.Modified;
 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UsuariosExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!UsuariosExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
+			return StatusCode(HttpStatusCode.NoContent);
+		}
 
-        // POST: api/Usuarios
-        //[ResponseType(typeof(Usuarios))]
-        public IHttpActionResult PostUsuarios(Usuarios usuarios)
-        {
-            if (!ModelState.IsValid)
-            {
-				if(ModelState.Keys.First().ToString() != "usuarios.Id")
-                return BadRequest(ModelState);
-            }
+		// POST: api/Usuarios
+		//[ResponseType(typeof(Usuarios))]
+		public IHttpActionResult PostUsuarios(Usuarios usuarios)
+		{
+			if (!ModelState.IsValid)
+			{
+				if (ModelState.Keys.First().ToString() != "usuarios.Id")
+					return BadRequest(ModelState);
+			}
 
-            db.Usuarios.Add(usuarios);
-            db.SaveChanges();
+			db.Usuarios.Add(usuarios);
+			db.SaveChanges();
 
-            return CreatedAtRoute("DefaultApi", new { id = usuarios.Id }, usuarios);
-        }
+			return CreatedAtRoute("DefaultApi", new { id = usuarios.Id }, usuarios);
+		}
 
-        // DELETE: api/Usuarios/5
-        [ResponseType(typeof(Usuarios))]
-        public IHttpActionResult DeleteUsuarios(int id)
-        {
-            Usuarios usuarios = db.Usuarios.Find(id);
-            if (usuarios == null)
-            {
-                return NotFound();
-            }
+		// DELETE: api/Usuarios/5
+		[ResponseType(typeof(Usuarios))]
+		public IHttpActionResult DeleteUsuarios(int id)
+		{
+			Usuarios usuarios = db.Usuarios.Find(id);
+			if (usuarios == null)
+			{
+				return NotFound();
+			}
 
-            usuarios.Ativo = false;
+			usuarios.Ativo = false;
 
-            db.SaveChanges();
+			db.SaveChanges();
 
-            return Ok(usuarios);
-        }
+			return Ok(usuarios);
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        private bool UsuariosExists(int id)
-        {
-            return db.Usuarios.Count(e => e.Id == id) > 0;
-        }
-    }
+		private bool UsuariosExists(int id)
+		{
+			return db.Usuarios.Count(e => e.Id == id) > 0;
+		}
+	}
 }
